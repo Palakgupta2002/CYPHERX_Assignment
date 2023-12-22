e77959156c2bb977b0e9666729a7455704a24d89
Palakgupta2002 <pg948303@gmail.com>
2023-12-22 21:16:26 +0530

    Process

--- Client/vite-project/src/Components/Home/Home.jsx
+++ Client/vite-project/src/Components/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../NavBar/Navbar'
 import InfoCard from '../Card/InfoCard'
+import { useContext,useState } from 'react'
+import InfoContext from '../../Config/Context'
 
 
 const Home = () => {
+    
     return (
         <>
+        <InfoContext.Provider>
             <Navbar />
             <InfoCard />
+        </InfoContext.Provider>
         </>
 
     )
--- Client/vite-project/src/Components/NavBar/Navbar.jsx
+++ Client/vite-project/src/Components/NavBar/Navbar.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Popover } from 'antd';
+import { ControlOutlined, UpOutlined, DownOutlined } from '@ant-design/icons'
+import "./Navbar.css"
+import Moon from "../../assets/Moon.svg"
+import Sun from "../../assets/Sun.svg"
+
+
 
 const Navbar = () => {
-  return (
-    <div className='navbarContainer'>
-        <div className="filterBtn">
-            
+    const [Grouping,setGrouping]=useState("Priority")
+    const [Ordering,setOrdering]=useState("Title")
+    const [open, setOpen] = useState(false);
+    const hide = () => {
+        setOpen(false);
+    };
+    const handleOpenChange = (newOpen) => {
+        setOpen(newOpen);
+    };
+    const handleGrouping=(event)=>{
+        setGrouping(event.target.value)
+    }
+    const handleOrdering=(event)=>{
+        setOrdering(event.target.value)
+    }
+
+    const content=(
+        <div className='popUpContainer'>
+            <div>
+                <div>Grouping</div>
+                <div>
+                    <select onChange={handleGrouping} value={Grouping} name="" id="">
+                        <option value="Priority">Priority</option>
+                        <option value="Users">Users</option>
+                        <option value="Status">Status</option>
+                    </select>
+                </div>
+            </div>
+            <div>
+                <div>Ordering</div>
+                <div>
+                    <select onChange={handleOrdering} value={Ordering} name="" id="">
+                        <option value="Priority">Priority</option>
+                        <option value="Title">Title</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div className="themeBtn"></div>
-    
-    </div>
-  )
+    )
+    return (
+        <div className='navbarContainer'>
+            <div className="filterBtn">
+                <Popover
+                    content={content}
+                    trigger="click"
+                    open={open}
+                    onOpenChange={handleOpenChange}
+                >
+                    <div className='displayContainer'>
+                        <span className="settingIcon"><ControlOutlined /></span>
+                        <span>Display</span>
+                        <span className="upAndDownIcon">
+                            {open ? <UpOutlined /> : <DownOutlined />}
+                        </span>
+                    </div>
+                </Popover>
+
+
+            </div>
+            <div className="themeBtn">
+                <img src={Moon} width={"20px"} />
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
